da40579bd986c2d41cdf8763aa8ddc745792db26
huukien2002 <kienlh@mumesoft.vn>
2025-04-30 20:16:05 +0700

    update

--- next.config.js
+++ next.config.js
@@ -19,7 +19,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/cms' : '',  assetPrefix: isProd ? '/cms/' : '',
   images: {
     unoptimized: true, // nếu bạn dùng <Image />
   },
